0170fd2d86b53b7e9d03cb554528cacbfe275bce
JasonKosasih <2172012@maranatha.ac.id>
2025-10-31 02:54:14 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/home', function () {
     return view('homepage.home');
 })->middleware(['auth', 'verified'])->name('homepage.home');
@@ -13,33 +17,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/', function () {
-    return view('homepage.home');
-})->name('home');
-
-// Route untuk halaman About
-Route::get('/about', function () {
-    return view('homepage.about');
-})->name('about'); // <-- Tambahkan nama route
-
-// Route untuk halaman Contact
-Route::get('/contact', function () {
-    return view('homepage.contact');
-})->name('contact');
-
-// Route untuk halaman Shop
-Route::get('/shop', function () {
-    return view('homepage.shop');
-})->name('shop'); // <-- Tambahkan nama route
-
-// Route untuk halaman detail produk (Shop Single)
-Route::get('/shop-single', function () {
-    return view('homepage.shop-single'); // Mengarahkan ke resources/views/homepages/shop-single.blade.php
-})->name('shop-single'); // <-- Tambahkan nama route
-
-// Route untuk halaman Wishlist
-Route::get('/wishlist', function () {
-    return view('homepage.wishlist'); // Mengarahkan ke resources/views/homepages/wishlist.blade.php
-})->name('wishlist');
-
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
